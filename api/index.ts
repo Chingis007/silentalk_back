4ac28218e6e649156fde69c0dcf4e2763b1413b9
Chingis007 <karaulnekerell@gmail.com>
2024-03-08 16:14:50 +0200

    п

--- api/index.ts
+++ api/index.ts
@@ -6,6 +6,8 @@ const { OAuth2Client } = require("google-auth-library")
 const cors = require("cors")
 const dotenv = require("dotenv").config()
 const MemoryStore = require("memorystore")(expressSession)
+export const fetchCache = "force-no-store"
+
 const app = express()
 
 // "builds": [{ "src": "/index.js", "use": "@vercel/node" }],
